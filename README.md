a13e567666f9a12e72da8ab56e934b999f23a7e0
snagov89 <102862648+snagov89@users.noreply.github.com>
2025-03-23 10:44:22 +0000

    Deploying to gh-pages from @ sdsecuorg/sdsecu_website@f4a1ccfc6f11ccb734feae544725e00088d58b0b 🚀

---
+++ README.md
@@ -0,0 +1,8 @@
+## SdSecu protfolio
+
+```
+Main sdsecu website. Used to display who we are as a group and what we do.
+```
+
+#### Feel free to modify it but please test before committing
+
---
+++ static/js/script.js
@@ -0,0 +1,28 @@
+
+$('#aboutUs').click(function() {
+    window.location.href = 'index.html';
+});
+
+
+$('#members').click(function() {
+    window.location.href = 'membres.html';
+});
+
+
+$('#join').click(function() {
+    window.location.href = 'join.html';
+});
+
+
+$('#writeups').click(function() {
+    window.location.href = 'writeups.html';
+});
+
+
+$('#projects').click(function() {
+    window.location.href = 'projects.html';
+});
+
+$('#contact').click(function() {
+    window.location.href = 'contact.html';
+});
