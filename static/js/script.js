dd61f8e44756942cfe955f7154c0e1d99f6b753c
snagov89 <102862648+snagov89@users.noreply.github.com>
2025-04-07 21:19:40 +0000

    Deploying to gh-pages from @ sdsecuorg/sdsecu_website@73ae4738f770ac3debe6ca5a2714fc40754b8975 🚀

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,42 @@
-
-$('#aboutUs').click(function() {
-    window.location.href = 'index.html';
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header-placeholder').innerHTML = data;
+      
+      $('#aboutUs').click(function() {
+        window.location.href = 'index.html';
+      });
+      $('#members').click(function() {
+        window.location.href = 'membres.html';
+      });
+      $('#join').click(function() {
+        window.location.href = 'join.html';
+      });
+      $('#writeups').click(function() {
+        window.location.href = 'writeups.html';
+      });
+      $('#projects').click(function() {
+        window.location.href = 'projects.html';
+      });
+      $('#contact').click(function() {
+        window.location.href = 'contact.html';
+      });
+      
+      const navToggle = document.querySelector('.nav-toggle');
+      const navContainer = document.querySelector('.nav-container');
+      
+      navToggle.addEventListener('click', function() {
+        navContainer.classList.toggle('active');
+      });
+    })
+    .catch(error => console.error('Error loading header:', error));
+  
+  fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading footer:', error));
 });
-
-
-$('#members').click(function() {
-    window.location.href = 'membres.html';
-});
-
-
-$('#join').click(function() {
-    window.location.href = 'join.html';
-});
-
-
-$('#writeups').click(function() {
-    window.location.href = 'writeups.html';
-});
-
-
-$('#projects').click(function() {
-    window.location.href = 'projects.html';
-});
-
-$('#contact').click(function() {
-    window.location.href = 'contact.html';
-});
-
 
